0f831d16e40c987999cabaca1184696ab5625268
Rustam Kamalov <kamrus97@mail.ru>
2022-01-30 22:18:46 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp16_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += AnimatingControl_Loaded;
+        }
+
+        private void AnimatingControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = FindResource("sb") as Storyboard;
+            sb.Begin();
+        }
+    }
+}
